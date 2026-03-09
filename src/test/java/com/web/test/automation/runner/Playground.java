5bf09465fa16f33719a25af49d2334449b5405ad
vinoth244 <vinoth.ks@webknot.in>
2026-03-09 14:35:06 +0530

    09-03-2026
    Vinoth to main

--- src/test/java/com/web/test/automation/runner/Playground.java
+++ src/test/java/com/web/test/automation/runner/Playground.java
@@ -16,8 +16,8 @@ import io.cucumber.testng.TestNGCucumberRunner;
 
 @Listeners({ SuiteEvent.class, PageEvent.class })
 
-@CucumberOptions(features = { "src.test.java.com.web.test.automation.feature" }, glue = {
-		"com/web/test/automation/hooks", "com.web.test.automation.steps_definitions" }, tags = "@AgencySignIn", 
+@CucumberOptions(features = { "src/test/java/com/web/test/automation/feature" }, glue = {
+		"com.web.test.automation.hooks", "com.web.test.automation.steps_definitions" }, tags = "@AdminSignIn", 
 				plugin = {
 				"pretty", "html:target/site/cucumber-pretty/cucumber.html", "json:target/cucumber/cucumber.json",
 				"rerun:target/rerun.txt",
